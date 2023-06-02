254267dab9f5c513ddb14ab308b11bae78c7ad63
AboutDelltime <lm144s76@gmail.com>
2023-06-01 18:11:43 -0700

    Updated Readme

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# Challenge_3_password_genorator
+# Challenge_3_password_generator
+I WANT to randomly generate a password that meets certain criteria
+SO THAT I can create a strong password that provides greater security
+
+Resolved a password generator that is stating undefine. 
+
+Add code that will generate a strong password thats at least 8 characters. 
+
--- script.js
+++ script.js
@@ -1,10 +1,23 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+function generatePassword() {
+  console.log ("You clicked the Button");
+
+  return password;
+}
+var chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYX!@#$%&*";
+var passwordLength = 12;
+var password = "";
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
+  
+  for(var i =0; i <= passwordLength; i++){
+    var randomNumber = Math.floor(Math.random() * chars.length);
+    password += chars.substring(randomNumber, randomNumber +1);
+  }
 
   passwordText.value = password;
 
